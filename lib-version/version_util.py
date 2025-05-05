a7bfbd7a4da50212a97ce48d42898074c9d230ef
Monica Păun <m.a.paun@student.tudelft.nl>
2025-05-05 19:08:45 +0200

    Adapt to python package naming

--- lib-version/version_util.py
+++
@@ -1,9 +0,0 @@
-import importlib.metadata
-
-class VersionUtil:
-
-    def get_version(package_name: str = "lib-version") -> str:
-        try:
-            return importlib.metadata.version(package_name)
-        except ImportError:
-            return "unknown"
